c82447cc5fb34623bf8bc6f21f2a8d76090d5973
andiz <andiz@sfu.ca>
2019-02-09 14:47:09 -0800

    More changes to dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -5,4 +5,4 @@ $dbUsername = "epiz_23430865";
 $dbPassword = "hackthe90s";
 $dbName = "epiz_23430865_forum";
 
-conn = mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
