2cea5d627a82166f5266fccc57b0bcedf70eddb7
bca69 <bca69@sfu.ca>
2019-02-09 12:59:40 -0800

    bruh

--- script/CreatePost.js
+++
@@ -1,8 +0,0 @@
-// fucntions for opening and closing pop-up form
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
