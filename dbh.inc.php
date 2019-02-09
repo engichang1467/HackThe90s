7cf130f9081cbd5baf5b75ed0a37e99b88f59c67
andiz <andiz@sfu.ca>
2019-02-09 14:45:14 -0800

    added a connectivity php file

---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "sql107.epizy.com";
+$dbUsername = "epiz_23430865";
+$dbPassword = "hackthe90s"
+$dbName = "epiz_23430865_forum";
+
+conn = mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
