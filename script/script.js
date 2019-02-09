70139e6511b30270298e309d29bb74025a7a0028
Samsung <samsung@ubuntu.ubuntu-domain>
2019-02-09 11:36:43 -0800

    add my stuff here

--- script/script.js
+++ script/script.js
@@ -4,3 +4,12 @@ function ask_user ()
 	var user = prompt("What's your name: ");
 	alert("Hi! " + user + " Welcome to my blog");
 }
+
+// fucntions for opening and closing pop-up form
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
