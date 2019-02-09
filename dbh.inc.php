d8001c306f1e387718fca078f0c35c2a45d00643
andiz <andiz@sfu.ca>
2019-02-09 14:45:55 -0800

    corrected dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "sql107.epizy.com";
 $dbUsername = "epiz_23430865";
-$dbPassword = "hackthe90s"
+$dbPassword = "hackthe90s";
 $dbName = "epiz_23430865_forum";
 
 conn = mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
